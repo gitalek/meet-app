7846bb2f0744bcf8c85dc357f96a69ee9a98f448
Alexander <shalek.edu@mail.ru>
2020-06-29 17:03:36 +1000

    refactor(Model): validate email format to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
-  validates :title, :description, :location, :organizer_email, presence: true
-  validates :title, length: { maximum: 75 }
+  validates :description, :location, presence: true
+  validates :title, presence: true, length: { maximum: 75 }
+  validates :organizer_email, presence: true, 'valid_email_2/email': true
 end
