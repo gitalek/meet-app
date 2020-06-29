af11ba5326c0a813aba2300dc0dafd958a6577c7
Alexander <shalek.edu@mail.ru>
2020-06-29 22:32:33 +1000

    feature(ui): add localization

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # ?! custom
+  config.hosts << 'ru.meetapp.local'
+  config.hosts << 'en.meetapp.local'
 end
