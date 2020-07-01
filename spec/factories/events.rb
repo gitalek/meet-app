f10e99bd086949d0e021b2824046e50a1bcff698
Alexander <shalek.edu@mail.ru>
2020-07-01 18:46:25 +1000

    testing(Event resource): fill factory attributes

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -5,10 +5,15 @@ FactoryBot.define do
     title { Faker::Book.title }
     description { Faker::Quote.matz }
     location { Faker::Address.city }
-    start_time {}
-    end_time {}
+    start_time { Faker::Date.forward(days: 30) }
+    end_time do
+      Faker::Date.between(
+        from: start_time,
+        to: start_time + 30.days
+      )
+    end
     organizer_email { Faker::Internet.email }
-    organizer_telegram {}
-    link {}
+    organizer_telegram { "@#{Faker::Internet.username(specifier: 5..8)}" }
+    link { Faker::Internet.url }
   end
 end
