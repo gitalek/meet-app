bcb1013380aa3c97193f9bdc008c8e9c639d9b44
Alexander <shalek.edu@mail.ru>
2020-08-19 14:27:37 +1000

    update User model: add username field, default username method; make migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :events
+
+  validates :username, presence: true, length: { maximum: 40 }
+
+  before_validation :set_new_user_random_username, on: :create
+
+  private
+
+  def set_new_user_random_username
+    print 'Before if'
+    if username.blank?
+      rand_name = "#{Faker::Creature::Animal.name}-#{Faker::Number.number(digits: 10)}"
+      self.username = rand_name
+      print 'Into if'
+    end
+  end
 end
